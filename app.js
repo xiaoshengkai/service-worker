fb827c61921177ec088c6758a35dd86a01de5398
小盛开同学 <354830981@qq.com>
2018-10-09 10:49:04 +0800

    23511

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-var headers = new Headers();
-// *** I set the header in order to solve the error above:
-// *** The value is set to "/" because this js is included in html file in upper folder.
-// *** I tried even "../" and many more others values...
-headers.append('Service-Worker-Allowed', '/service-worker');
-console.log(headers.get('Service-Worker-Allowed'));
-
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker/sw.js')
